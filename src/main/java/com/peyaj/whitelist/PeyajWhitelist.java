54bb10fa5d24c4f948f9fa6d072a958bc1324a4d
realpeyaj <realpeyaj@users.noreply.github.com>
2026-07-04 15:30:00 +0800

    Bump plugin version to 3.0

--- src/main/java/com/peyaj/whitelist/PeyajWhitelist.java
+++ src/main/java/com/peyaj/whitelist/PeyajWhitelist.java
@@ -101,7 +101,7 @@ public class PeyajWhitelist extends JavaPlugin {
     private void printBanner() {
         boolean floodgateActive = getServer().getPluginManager().isPluginEnabled("Floodgate");
         boolean luckpermsActive = getServer().getPluginManager().isPluginEnabled("LuckPerms");
-        getLogger().info("▲ PeyajWhitelist v2.0");
+        getLogger().info("▲ PeyajWhitelist v3.0");
         getLogger().info("▪ Whitelist: " + (whitelistEnabled ? "§aEnabled" : "§cDisabled"));
         getLogger().info("▪ Maintenance: " + (maintenanceMode ? "§cEnabled" : "§aDisabled"));
         getLogger().info("▪ Crossplay: " + (floodgateActive ? "§aActive (Geyser/Floodgate)" : "§7Inactive"));
@@ -295,7 +295,7 @@ public class PeyajWhitelist extends JavaPlugin {
         }
 
         com.google.gson.JsonObject footer = new com.google.gson.JsonObject();
-        footer.addProperty("text", "PeyajWhitelist v2.0");
+        footer.addProperty("text", "PeyajWhitelist v3.0");
         embed.add("footer", footer);
 
         embeds.add(embed);
